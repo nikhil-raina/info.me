1c483ff612821f4cec13a5fa35b1d66588335c29
Nikhil Raina <nxr5013@rit.edu>
2020-05-26 03:16:57 -0700

    Made the welcome screen as the start of the app

--- portfolio/index.js
+++ portfolio/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './ts/Home/App';
+import WelcomeScreen from './ts/WelcomeScreen/WelcomeScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => WelcomeScreen);
