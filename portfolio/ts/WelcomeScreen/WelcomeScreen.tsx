527be79cd4af18d4fe14700f15e9e35578c839b3
Nikhil Raina <nxr5013@rit.edu>
2020-05-26 03:14:32 -0700

    Changed the entire class to say hello

--- portfolio/ts/WelcomeScreen/WelcomeScreen.tsx
+++ portfolio/ts/WelcomeScreen/WelcomeScreen.tsx
@@ -5,71 +5,14 @@
 
 import React from 'react';
 import styles from './WelcomeScreenStyle';
-import {View, Text, Button} from 'react-native';
 
-export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
-}
+import {View, Text} from 'react-native';
 
-interface State {
-  enthusiasmLevel: number;
-}
-
-export default class WelcomeScreen extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    if ((props.enthusiasmLevel || 0) <= 0) {
-      throw new Error('You could be a little more enthusiastic. :D');
-    }
-
-    this.state = {
-      enthusiasmLevel: props.enthusiasmLevel || 1,
-    };
-  }
-
-  onIncrement = () => {
-    this.setState({
-      enthusiasmLevel: this.state.enthusiasmLevel + 1,
-    });
-  };
-
-  onDecrement = () => {
-    this.setState({
-      enthusiasmLevel: this.state.enthusiasmLevel - 1,
-    });
-  };
-
-  getExclamationMarks = (numChars: number) => Array(numChars + 1).join('!');
-
+export default class WelcomeScreen extends React.Component {
   render(): React.ReactElement {
     return (
-      <View style={styles.root}>
-        <Text style={styles.greeting}>
-          Hello{' '}
-          {this.props.name +
-            this.getExclamationMarks(this.state.enthusiasmLevel)}
-        </Text>
-
-        <View style={styles.buttons}>
-          <View style={styles.button}>
-            <Button
-              title="-"
-              onPress={this.onDecrement}
-              accessibilityLabel="decrement"
-              color="red"
-            />
-          </View>
-
-          <View style={styles.button}>
-            <Button
-              title="+"
-              onPress={this.onIncrement}
-              accessibilityLabel="increment"
-              color="blue"
-            />
-          </View>
-        </View>
+      <View style={styles.container}>
+        <Text>Hello</Text>
       </View>
     );
   }
