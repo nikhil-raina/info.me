3827c87b6f68ac2626538ab3a38f403de52c01fb
Nikhil Raina <nxr5013@rit.edu>
2020-05-26 20:26:05 -0700

    Made Login Screen as the starting point for testing

--- portfolio/index.js
+++ portfolio/index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './ts/Home/App';
 import WelcomeScreen from './ts/WelcomeScreen/WelcomeScreen';
+import LoginScreen from './ts/LoginScreen/LoginScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => WelcomeScreen);
+AppRegistry.registerComponent(appName, () => LoginScreen);
