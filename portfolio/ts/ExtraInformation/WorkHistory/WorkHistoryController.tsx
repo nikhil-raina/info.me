4e6d2cfdd66dd483304e88b39e9bd27b6c9c7c92
Nikhil Raina <nxr5013@rit.edu>
2020-06-21 18:35:44 -0700

    Imported the experience

--- portfolio/ts/ExtraInformation/WorkHistory/WorkHistoryController.tsx
+++ portfolio/ts/ExtraInformation/WorkHistory/WorkHistoryController.tsx
@@ -5,19 +5,25 @@
  */
 
 import React from 'react';
-import WorkHistoryScreen from './WorkHistoryScreen/WorkHistoryScreen';
+import Experience from '../../ObjectModules/INFExperience';
 import WorkHistory from '../../ObjectModules/INFWorkHistory';
+import WorkHistoryScreen from './WorkHistoryScreen/WorkHistoryScreen';
+import * as data from '../../DataFiles/workHistoryData.json';
+
 
 class WorkHistoryController {
 
-  jobs: Array<WorkHistory>;
+  jobs: Array<Experience>;
+  workHistoryData: Array<WorkHistory>;
+
   // TODO get all the data from the API call for the work history and
   // pass it in the work history component screen for it to be printed.
   constructor() {
     this.jobs = [];
+    this.workHistoryData = [];
   }
 
-  getJobs() {
+  getJobs(workId: number) {
 
   }
 
