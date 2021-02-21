35895ce94cf676cf410e54f32eec468fe77e11ea
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-02-21 04:52:09 -0500

    Stubbed it for now

---
+++ portfolio/ts/Home/HomeScreenController.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @providesModule WorkHistoryScreenController
+ * @flow
+ */
+
+import React from 'react';
+import WorkHistoryScreen from './HomeScreen/HomeScreen';
+
+/*
+TODO: create a enum having the 5 qualities here. This should later be able to 
+correspond to the obvious objects.
+*/
+
+
+class WorkHistoryController {
+
+
+  constructor() {
+
+  }
+
+  getQualities(): null {
+    return null;
+  }
+
+}
+
+export default new WorkHistoryController();
