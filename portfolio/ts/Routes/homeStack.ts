180701547aa448fafd7311197e1164c6e4505b0b
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-02-21 04:52:51 -0500

    Updating the directory path

--- portfolio/ts/Routes/homeStack.ts
+++ portfolio/ts/Routes/homeStack.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import HomeScreen from '../HomeScreen/HomeScreen';
+import HomeScreen from '../Home/HomeScreen/HomeScreen';
 import LoginScreen from '../LoginScreen/LoginScreen';
 
 const screens = {
